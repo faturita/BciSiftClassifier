662952f0e70ff29e1d6df3df9ae55aba19b87b72
faturita <rramele@gmail.com>
2015-10-16 14:08:41 -0300

    Change to CrossValidation

--- CrossValidation.m
+++ CrossValidation.m
@@ -1,36 +1,52 @@
 % Parameters ==============================
-DbScanRadio=110;minPts=2;channel=7;graphics=1; comps=0; 
+DbScanRadio=210;minPts=2;channel=7;graphics=0; comps=0; 
 %trainingRange=epochRange; %[1:10 16:25];
 %testRange=epochRange; %[11:15 26:30];
-channelRange=12:12;
+channelRange=1:14;
 DbScanRadioRange=210:210;  % Useless!
 prompt = 'Experiment? ';
 expcode = input(prompt);
 %==========================================
 
-T=40;
+for channel=channelRange
+T=3;
+KFolds=3;
+E = zeros(T,1);
 
 for t=1:T
 
-kfolds = fold(10, epochRange);
+kfolds = fold(KFolds, epochRange);
 
-N = zeros(10,1);
+N = zeros(KFolds,1);
 
-for f=1:10
+for f=1:KFolds
 
     trainingRange=defold(kfolds, f);
     testRange=kfolds{f};
     
     % --------------------------
     Performance=[];
-    for channel=channelRange
-            fprintf('Channel %10.3f - MinPts %d - Radio: %10.3f\n', channel,minPts, DbScanRadio);
-            run('BciSiftNNBNFeatureExtractor.m');
-            run('BciSiftNNBNClassifier.m');
-            Performance(channel, 1)= ACC;
+    %for channel=channelRange
+        fprintf('Channel %d - MinPts %d - Radio: %10.3f\n', channel,minPts, DbScanRadio);
+        %run('BciSiftNNBNFeatureExtractor.m');
+        %run('BciSiftNNBNClassifier.m');
+        %Performance(channel, 1)= ACC;
+
+        %N(f) = ERR;
             
-            N(f) = ACC;
-    end
+        DE = BciSiftNBNNFeatureExtractor(F,expcode,channel,trainingRange,labelRange,graphics);
+        [ACC, ERR, SC] = BciSiftNBNNClassifier(F,DE,channel,testRange,labelRange,0,0);
+        Performance(channel, 1)= ACC;
+        N(f) = ERR;
+        
+        %fprintf('Channel %10.3f - MinPts %d - Radio: %10.3f\n', channel,minPts, DbScanRadio);
+        %DE = BciSiftFeatureExtractor(F,expcode,DbScanRadio,minPts,channel,trainingRange,labelRange,0,0);
+        %[ACC, ERR, SC] = BciSiftClassifier(F,DE,channel,testRange,labelRange,0,0);
+        %Performance(channel, DbScanRadio)= ACC; 
+        
+        %N(f) = ERR;
+        %ERR
+    %end
 
 
     if (graphics)
@@ -46,11 +62,20 @@ for f=1:10
 
 end
 
-E(t) = sum(N)/size(epochRange,2)/2;
+E(t) = sum(N)/size(epochRange,2);
 
 end
 
 e= sum(E)/T;
 V = (sum( E - e )^2)  / (T-1);
 sigma = sqrt( V );
+ErrorPerChannel(channel)=e;
+end
 
+
+        figure
+        plot(ErrorPerChannel(channel,:));
+        title(sprintf('Exp.%d:k-fold Cross Validation NBNN',expcode));
+        xlabel('Channel')
+        ylabel('Error')
+        axis([0 16 0 1.3]);
